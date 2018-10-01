bc1b9cfecddbafde311e681442a1a3e59a5eced0
TJ Martin <tmartin2089@gmail.com>
2018-10-01 15:22:34 -0500

    start on 320x50

---
+++ src/ads/320x50/js/Custom.js
@@ -0,0 +1,13 @@
+/* Custom.js */
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // wait until window, stylesheets, images, links, and other media assets are loaded
+  window.onload = function() {
+
+    // All ready, start!
+    var ad = new Ad({name:"mpu"});
+
+  };
+});
+/* End Custom.js */
